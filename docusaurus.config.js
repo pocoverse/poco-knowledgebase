d493834a7929582a3fd5084dd054df0f0256fed8
vcpoco <vedic.c@pococare.com>
2023-02-06 17:32:05 +0530

    Updated the docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,9 +18,11 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'vcpoco', // Usually your GitHub org/user name.
+  projectName: 'poco-knowledgebase', // Usually your repo name.
+  trailingSlash: false,
 
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
