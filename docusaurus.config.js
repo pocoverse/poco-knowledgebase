d39c980e261bed9c2f56f5b538faa68bfd687fec
Shantanu-b <69183403+Shantanu-b@users.noreply.github.com>
2023-02-07 14:01:54 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/pococarelogo.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.pococare.com',
+  url: 'https://vcpoco.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
