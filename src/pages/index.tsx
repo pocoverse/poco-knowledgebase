87840292ac4f410e9020ee63ac16db126d65987e
Shantanu-b <69183403+Shantanu-b@users.noreply.github.com>
2023-02-08 12:49:34 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from 'https://vcpoco.github.io/poco-knowledgebase/src/components/HomepageFeatures';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
